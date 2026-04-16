b30e75303e4df5c1590bd02064600987a159eee6
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-04-16 19:54:33 +0600

    global error handler

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,20 @@
+import type { NextFunction, Request, Response } from "express";
+import { AppError } from "../utills/error";
+
+
+export const globalErrorHandler=(err:any,req:Request,res:Response,next:NextFunction)=>{
+    if(err instanceof  AppError){
+     return res.status( 500).json({
+      success: false,
+      message: err.message,
+    });
+
+    }
+
+    console.log(err)
+    return res.status(500).json({
+        success: false,
+        message: "something went wrong",
+        errror: err,
+    })
+}
