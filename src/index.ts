11717cd3ac7dd6d2b2085e37e22ca0a29b227542
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-04-20 11:49:07 +0600

    auth limiter and global limiter

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,30 @@ import { globalErrorHandler } from "./middleware/error.middleware";
 import login from "./modules/auth/auth.routes";
 import { loginValidation } from "./middleware/auth.middleware";
 import messageRoutes from "./modules/message/message.routes";
+import helmet from "helmet";
+import morgan from "morgan";
+import { authLimiter, globalLimiter } from "./middleware/security.middleware";
+
 
 dotenv.config();
 
 const app = express();
 
+app.use(helmet())
+
 app.use(cors());
 app.use(express.json());
+
+app.use(morgan("dev"))
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/",loginValidation, (req, res) => {
   res.send("Server is running 🚀");
 });
-
-app.use("/api/auth",signUp)
-app.use("/api/auth",login)
+app.use(globalLimiter)
+app.use("/api/auth",authLimiter,signUp)
+app.use("/api/auth",authLimiter,login)
 app.use("/api/message",messageRoutes)
 
 app.use(globalErrorHandler)
--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,8 +1,10 @@
+import 'dotenv/config'
 import type { NextFunction, Request, Response } from "express";
 import { AppError } from "../utills/error";
 
 
 export const globalErrorHandler=(err:any,req:Request,res:Response,next:NextFunction)=>{
+      const statusCode = err.statusCode || 500;
     if(err instanceof  AppError){
      return res.status( 500).json({
       success: false,
@@ -10,6 +12,12 @@ export const globalErrorHandler=(err:any,req:Request,res:Response,next:NextFunct
     });
 
     }
+    if (process.env.NODE_ENV === "production") {
+    return res.status(statusCode).json({
+      success: false,
+      message: err.message || "Something went wrong",
+    });
+  }
 
     return res.status(500).json({
         success: false,
