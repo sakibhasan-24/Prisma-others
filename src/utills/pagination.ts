44645ee0f741044372259c4f04ce42e8cbc08815
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-04-21 20:28:49 +0600

    pagination reusable function

---
+++ src/utills/pagination.ts
@@ -0,0 +1,16 @@
+export const getPagination = (query: any) => {
+  const page = Math.max(Number(query.page) || 1, 1);
+  const limit = Math.min(Number(query.limit) || 10, 100);
+  const skip = (page - 1) * limit;
+
+  const sortBy = query.sortBy || "createdAt";
+  const sortOrder = query.sortOrder === "asc" ? "asc" : "desc";
+
+  return {
+    page,
+    limit,
+    skip,
+    sortBy,
+    sortOrder,
+  };
+};
